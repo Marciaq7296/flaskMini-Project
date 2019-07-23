85bf134776b5aad34cd65480cc999dbb700499dd
EC2 Default User <ec2-user@ip-172-31-25-35.us-east-2.compute.internal>
2019-07-23 19:26:27 +0000

    second commit

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,15 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    return render_template("index.html")
+    
+@app.route('/tipAmount', methods = ["GET","POST"])
+def tipAmount():
+    if request.method == 'GET':
+         return "You did not fill out the form"
+    else:
+        userData = dict(request.form)
+        billAmount = userData['billAmount'][0]
+        percent = userData['percent'][0]
+        tipNum = model.tip(percent, billAmount)
+        return render_template("tipAmount.html", billAmount = billAmount, percent = percent, tipNum = tipNum)
